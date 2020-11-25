15c3f4d33d4237e94f6d393f28470fab5ac6691e
Jose Miralles <josemiralles1991@gmail.com>
2020-11-24 21:31:02 -0500

    applied toys migration, and added annotate gem

--- Week_5/W5D4/DoggyDayCare/db/schema.rb
+++ Week_5/W5D4/DoggyDayCare/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_014413) do
+ActiveRecord::Schema.define(version: 2020_11_25_015301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_014413) do
 
   create_table "toys", force: :cascade do |t|
     t.string "name", null: false
+    t.integer "dog_id", null: false
+    t.string "color", null: false
   end
 
 end
