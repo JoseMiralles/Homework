a16c1d8feb73f107737093f84230cc81e69bc036
Jose Miralles <josemiralles1991@gmail.com>
2020-12-06 20:12:31 -0500

    Finished HM

--- Week_7/W7D1/rails_auth/app/models/user.rb
+++ Week_7/W7D1/rails_auth/app/models/user.rb
@@ -1,10 +1,39 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+    # u = User.new({username:"Jose", password: "password1234"})
 
-    # A user password can be nil. But not a string smaller than 6 chars.
-    validates: password, length: {minimum: 6, allow_nill: true}
+    attr_reader :password
+  
+    validates :username, :password_digest, :session_token, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+  
+    before_validation :ensure_session_token
+  
+    # Return the user with the given credentials. Returns nil if the user wasn't found.
+    def self.find_by_credentials(username, password)
+      user = User.find_by(username: username) # Get the user with the give username.
+      return nil if user == nil # Return nil if the user wasn't found
 
-    def password=(pw)
-        self.password_digest =  BCrypt.password.create(password)
+      # Check if the given password hashes to the digest, and return the user if it does.
+      return user if user.password_digest.is_password?(password)
+      return nil # Wrong password, return nil.
     end
-
-end
+  
+    def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  
+    def password_digest
+      BCrypt::Password.new(super)
+    end
+  
+    # Creates a session token
+    def ensure_session_token
+      self.session_token = SecureRandom::urlsafe_base64 if self.session_token == nil
+    end
+  
+    def reset_session_token!
+      self.session_token = SecureRandom::urlsafe_base64
+      self.save!
+    end
+  end
