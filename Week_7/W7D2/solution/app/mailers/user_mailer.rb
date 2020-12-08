ad04f0e05dc9a18971941d141bdc854bd4ec681e
Jose Miralles <josemiralles1991@gmail.com>
2020-12-07 23:19:47 -0500

    Added Mailer

---
+++ Week_7/W7D2/solution/app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+    default from 'from@example.com'
+
+    def welcome_email(user)
+    
+        @user = user
+        @url = "http://example.com/login"
+        mail(to: user.email, subject: "Welcome to My Awesome Site.")
+    
+    end
+
+end
