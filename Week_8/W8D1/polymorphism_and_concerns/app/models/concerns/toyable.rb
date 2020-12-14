9db47bdd11f6a0d804c21c3809c3f30980e1e256
Jose Miralles <josemiralles1991@gmail.com>
2020-12-13 19:08:45 -0500

    I finished, I think

--- Week_8/W8D1/polymorphism_and_concerns/app/models/concerns/toyable.rb
+++ Week_8/W8D1/polymorphism_and_concerns/app/models/concerns/toyable.rb
@@ -5,9 +5,13 @@ module Toyable
         has_many :toys, foreign_key: :toyable_id, class_name: :Toy
     end
     
+    #Cat.first.receive_toy("test")
     def receive_toy(name)
-        toy = Toy.find_or_create_by(name: name)
-        self.toys.create()
+        toy = Toy.find_or_create_by(
+            name: name,
+            toyable_type: self.class.to_s,
+            toyable_id: self.id)
+        self.toys += [toy]
     end
 
 end
