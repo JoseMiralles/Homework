245558c26568a84143604b583167d9d16c7c6df6
Jose Miralles <josemiralles1991@gmail.com>
2020-12-30 19:19:29 -0500

    Finished homework.

--- Week_9/W9D4/First_Ajax_Request/js/index.js
+++ Week_9/W9D4/First_Ajax_Request/js/index.js
@@ -1,5 +1,15 @@
-console.log("Hello from the JavaScript console!");
+// console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+    method: "GET",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    dataType: "json",
+    success: function(r){
+        console.log(Object.values(r.weather[0]).toString());
+    },
+    error: ()=>{console.log("Failed")}
+});
 
 // Add another console log here, outside your AJAX request
+console.log("After the AJAX request.");
