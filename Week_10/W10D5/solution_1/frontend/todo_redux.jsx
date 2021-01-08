d80a1904b92addbb838ade986e68145d2f56e239
Jose <josemiralles1991@gmail.com>
2021-01-07 21:00:00 -0500

    Finished phase 1 of HW

---
+++ Week_10/W10D5/solution_1/frontend/todo_redux.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+
+  store.dispatch = addLoggingToDispatch(store);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+const addLoggingToDispatch = ( store ) => {
+  const dispatch = store.dispatch;
+  return action => {
+    console.log( store.getState() );
+    console.log( action );
+    dispatch( action );
+    console.log( store.getState() );
+  };
+}
